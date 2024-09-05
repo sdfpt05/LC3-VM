569c54a779ee682f7b562c938bd7a7085640d43d
kevin <kevin@novek.io>
2024-09-05 16:11:32 +0300

    feat:added memmory mapped registers

---
+++ lc3.h
@@ -0,0 +1,77 @@
+#ifndef LC3_H
+#define LC3_H
+
+#include <stdint.h>
+
+#define MEMORY_MAX (1 << 16)
+
+// Registers
+enum
+{
+    R_R0 = 0,
+    R_R1,
+    R_R2,
+    R_R3,
+    R_R4,
+    R_R5,
+    R_R6,
+    R_R7,
+    R_PC,
+    R_COND,
+    R_COUNT
+};
+
+// Opcodes
+enum
+{
+    OP_BR = 0,
+    OP_ADD,
+    OP_LD,
+    OP_ST,
+    OP_JSR,
+    OP_AND,
+    OP_LDR,
+    OP_STR,
+    OP_RTI,
+    OP_NOT,
+    OP_LDI,
+    OP_STI,
+    OP_JMP,
+    OP_RES,
+    OP_LEA,
+    OP_TRAP
+};
+
+// Condition Flags
+enum
+{
+    FL_POS = 1 << 0,
+    FL_ZRO = 1 << 1,
+    FL_NEG = 1 << 2,
+};
+
+// Trap Codes
+enum
+{
+    TRAP_GETC = 0x20,
+    TRAP_OUT = 0x21,
+    TRAP_PUTS = 0x22,
+    TRAP_IN = 0x23,
+    TRAP_PUTSP = 0x24,
+    TRAP_HALT = 0x25
+};
+
+// Memory Mapped Registers
+enum
+{
+    MR_KBSR = 0xFE00,
+    MR_KBDR = 0xFE02
+};
+
+// Function prototypes
+void lc3_init(void);
+int lc3_load_image(const char *image_path);
+void lc3_run(void);
+void lc3_cleanup(void);
+
+#endif // LC3_H
