f4d09b0b60b0df04e247faa15c02e48ddc3b905a
kevin <kevin@novek.io>
2024-09-05 16:04:03 +0300

    feat:added inst exec fuction types

--- main.c
+++ main.c
@@ -78,3 +78,40 @@ enum
     MR_KBSR = 0xFE00,
     MR_KBDR = 0xFE02
 };
+
+// Function prototypes
+static void update_flags(uint16_t r);
+static uint16_t sign_extend(uint16_t x, int bit_count);
+static uint16_t swap16(uint16_t x);
+static uint16_t check_key(void);
+static void mem_write(uint16_t address, uint16_t val);
+static uint16_t mem_read(uint16_t address);
+static int read_image(const char *image_path);
+static void handle_interrupt(int signal);
+static void disable_input_buffering(void);
+static void restore_input_buffering(void);
+
+// Improved error handling
+#define PRINT_ERROR(...) fprintf(stderr, "Error: " __VA_ARGS__)
+#define EXIT_WITH_ERROR(...)      \
+    do                            \
+    {                             \
+        PRINT_ERROR(__VA_ARGS__); \
+        exit(1);                  \
+    } while (0)
+
+// Instruction execution functions
+static void exec_add(uint16_t instr);
+static void exec_and(uint16_t instr);
+static void exec_br(uint16_t instr);
+static void exec_jmp(uint16_t instr);
+static void exec_jsr(uint16_t instr);
+static void exec_ld(uint16_t instr);
+static void exec_ldi(uint16_t instr);
+static void exec_ldr(uint16_t instr);
+static void exec_lea(uint16_t instr);
+static void exec_not(uint16_t instr);
+static void exec_st(uint16_t instr);
+static void exec_sti(uint16_t instr);
+static void exec_str(uint16_t instr);
+static void exec_trap(uint16_t instr);
