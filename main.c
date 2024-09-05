b139a92b833d82f76ddf1ec6e021438049f56f42
kevin <kevin@novek.io>
2024-09-05 16:05:27 +0300

    feat:defined op codes

--- main.c
+++ main.c
@@ -123,3 +123,96 @@ static void trap_puts(void);
 static void trap_in(void);
 static void trap_putsp(void);
 static void trap_halt(void);
+
+// Global variables for input buffering
+static struct termios original_tio;
+static volatile sig_atomic_t running = 1;
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        PRINT_ERROR("Usage: %s <image-file1> ...\n", argv[0]);
+        exit(2);
+    }
+
+    for (int j = 1; j < argc; ++j)
+    {
+        if (!read_image(argv[j]))
+        {
+            EXIT_WITH_ERROR("Failed to load image: %s\n", argv[j]);
+        }
+    }
+
+    signal(SIGINT, handle_interrupt);
+    disable_input_buffering();
+
+    // Set the PC to starting position
+    // 0x3000 is the default
+    enum
+    {
+        PC_START = 0x3000
+    };
+    reg[R_PC] = PC_START;
+
+    while (running)
+    {
+        uint16_t instr = mem_read(reg[R_PC]++);
+        uint16_t op = instr >> 12;
+
+        switch (op)
+        {
+        case OP_ADD:
+            exec_add(instr);
+            break;
+        case OP_AND:
+            exec_and(instr);
+            break;
+        case OP_BR:
+            exec_br(instr);
+            break;
+        case OP_JMP:
+            exec_jmp(instr);
+            break;
+        case OP_JSR:
+            exec_jsr(instr);
+            break;
+        case OP_LD:
+            exec_ld(instr);
+            break;
+        case OP_LDI:
+            exec_ldi(instr);
+            break;
+        case OP_LDR:
+            exec_ldr(instr);
+            break;
+        case OP_LEA:
+            exec_lea(instr);
+            break;
+        case OP_NOT:
+            exec_not(instr);
+            break;
+        case OP_ST:
+            exec_st(instr);
+            break;
+        case OP_STI:
+            exec_sti(instr);
+            break;
+        case OP_STR:
+            exec_str(instr);
+            break;
+        case OP_TRAP:
+            exec_trap(instr);
+            break;
+        case OP_RES:
+        case OP_RTI:
+        default:
+            PRINT_ERROR("BAD OPCODE: %d\n", op);
+            running = 0;
+            break;
+        }
+    }
+
+    restore_input_buffering();
+    return 0;
+}
